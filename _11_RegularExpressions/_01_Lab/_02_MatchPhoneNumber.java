b79103039e74c5125178116273ff3a2c7a62a88f
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-11-15 23:19:08 +0200

    add RegularExpressions-Lab

---
+++ _11_RegularExpressions/_01_Lab/_02_MatchPhoneNumber.java
@@ -0,0 +1,26 @@
+package _11_RegularExpressions._01_Lab;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class _02_MatchPhoneNumber {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        String text = scanner.nextLine();
+		
+        String regex = "\\+359([- ])2\\1[0-9]{3}\\1[0-9]{4}\\b";
+        Pattern pattern = Pattern.compile(regex); 
+        Matcher matcher = pattern.matcher(text); 
+
+        List<String> phoneNumbers = new ArrayList<>();
+        while (matcher.find()) {
+            phoneNumbers.add(matcher.group());
+        }
+        System.out.println(String.join(", ", phoneNumbers));
+    }
+}
