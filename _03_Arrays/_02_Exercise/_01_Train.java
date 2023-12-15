8fb02bd25386cd759199487508d7f88e30ea500c
Dimitar-Peev <143292168+Dimitar-Peev@users.noreply.github.com>
2023-12-15 09:09:09 +0200

    Update _01_Train.java

--- _03_Arrays/_02_Exercise/_01_Train.java
+++ _03_Arrays/_02_Exercise/_01_Train.java
@@ -5,18 +5,17 @@ import java.util.Scanner;
 public class _01_Train {
     public static void main(String[] args) {
 
-        Scanner scanner = new Scanner(System.in);
+       Scanner scanner = new Scanner(System.in);
 
         int wagons = Integer.parseInt(scanner.nextLine());
-        int[] train = new int[wagons];
+        int sumPeople = 0;
 
-        int allPeople = 0;
-        for (int i = 0; i < train.length; i++) {
-            train[i] = Integer.parseInt(scanner.nextLine());
-            allPeople += train[i];
-            System.out.print(train[i] + " ");
+        for (int i = 0; i < wagons; i++) {
+            int people = Integer.parseInt(scanner.nextLine());
+            System.out.print(people + " ");
+            sumPeople += people;
         }
 
-        System.out.printf("%n%s", allPeople);
+        System.out.println("\n" + sumPeople);
     }
 }
